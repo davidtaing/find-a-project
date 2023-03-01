23422aca6ce3b47d805995a035b3466dd0ed1e6d
davidtaing <adavidtaing@gmail.com>
2023-03-01 18:25:46 +1100

    build: add @tailwindcss/typography
    and update tailwind.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-red-500">
+    <main className="prose bg-red-500">
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
