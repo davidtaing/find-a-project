19115d47cefef700c4f0de0919dfa7ef4db02939
davidtaing <adavidtaing@gmail.com>
2023-03-01 22:55:53 +1100

    style: add mobile styling to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className="m-h-screen prose prose-xl  prose-stone w-full max-w-full dark:prose-invert "
+      className="m-h-screen md: prose  prose-xl prose-stone w-full max-w-full p-4 dark:prose-invert md:p-0"
       aria-label="Home Page"
     >
-      <section className="mx-auto grid h-96 max-w-6xl grid-cols-12 gap-6">
-        <div className="col-span-6 my-auto">
+      <section className="mx-auto my-20 grid h-96 max-w-6xl grid-cols-4 md:mb-0 lg:grid-cols-12 lg:gap-6">
+        <div className="col-span-4 my-auto text-center lg:col-span-6 lg:text-left">
           <h1 className="font-medium">Work towards your dream job</h1>
-          <div className="flex items-center gap-6">
+          <div className="flex flex-col-reverse items-center gap-3 lg:flex-row lg:gap-6">
             <button className="rounded border border-blue-600 bg-blue-600 px-3 py-2 text-stone-50 hover:bg-blue-800">
               Join a Project
             </button>
@@ -23,14 +23,14 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-12 gap-6">
-        <div className="col-span-12">
+      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-4 gap-6 lg:grid-cols-12">
+        <div className="col-span-4 lg:col-span-12">
           <h2 className="text-center">Featured Projects</h2>
         </div>
-        <article className="col-span-6 h-80 rounded-md border border-stone-200 bg-stone-100 dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 h-80 rounded-md border border-stone-200 bg-stone-100 dark:border-stone-800 dark:bg-stone-800 lg:col-span-6">
           Project 1
         </article>
-        <article className="col-span-6 h-80 rounded-md border border-stone-200 bg-stone-100 dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 h-80 rounded-md border border-stone-200 bg-stone-100 dark:border-stone-800 dark:bg-stone-800 lg:col-span-6">
           Project 2
         </article>
         <article className="col-span-4 h-80 rounded-md border border-stone-200 bg-stone-100 dark:border-stone-800 dark:bg-stone-800">
@@ -45,33 +45,33 @@ export default function Home() {
         </article>
       </section>
 
-      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-12 gap-6">
-        <div className="col-span-12">
+      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-4 gap-6 lg:grid-cols-12">
+        <div className="col-span-4 lg:col-span-12">
           <h2 className="text-center">Here&apos;s How You Can Win!</h2>
         </div>
-        <article className="col-span-3 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800 lg:col-span-3">
           <p className="m-0">🎯</p>
           <p className="m-0 font-medium">Build In-Demand Skills</p>
         </article>
-        <article className="col-span-3 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800 lg:col-span-3">
           <p className="m-0">💪</p>
           <p className="m-0 font-medium">Showcase Your Work</p>
         </article>
-        <article className="col-span-3 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800 lg:col-span-3">
           <p className="m-0">🤝</p>
           <p className="m-0 font-medium">Make New Friends</p>
         </article>
-        <article className="col-span-3 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800">
+        <article className="col-span-4 rounded-md border border-stone-300 bg-stone-100 p-6 text-center dark:border-stone-800 dark:bg-stone-800 lg:col-span-3">
           <p className="m-0">🧑‍🎓</p>
           <p className="m-0 font-medium">Work With Mentors</p>
         </article>
       </section>
 
-      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-12 gap-6">
-        <div className="col-span-12">
+      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-4 lg:grid-cols-12 lg:gap-6">
+        <div className="col-span-4 lg:col-span-12">
           <h2 className="text-center">About Find a Project</h2>
         </div>
-        <div className="col-span-6 col-start-4 rounded-md border border-stone-300 bg-stone-100 p-6 dark:border-stone-800 dark:bg-stone-800">
+        <div className="col-span-4 rounded-md border border-stone-300 bg-stone-100 p-6 dark:border-stone-800 dark:bg-stone-800 lg:col-span-6 lg:col-start-4">
           <div className="flex items-center gap-4 ">
             <Image
               className="m-0 rounded-full"
@@ -97,7 +97,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-12 gap-6">
+      <section className="mx-auto mb-20 grid max-w-6xl grid-cols-4 gap-6 lg:grid-cols-12">
         <div className="col-span-12 text-center">
           <h2>Join the community</h2>
           <p>
