cf4cdf34d845d692011e88ba6ef0eadb23dc3f4c
davidtaing <adavidtaing@gmail.com>
2023-04-01 22:26:32 +1100

    refactor: rename env var to CMS_DATABASE_URL
    to reflect global .env file

--- apps/cms/keystone.ts
+++ apps/cms/keystone.ts
@@ -21,7 +21,7 @@ export default withAuth(
       //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: "mysql",
       url:
-        process.env.DATABASE_URL ??
+        process.env.CMS_DATABASE_URL ??
         "mysql://root:dbpass@localhost:3306/find-a-project",
       prismaClientPath: "node_modules/.prisma/client",
     },
