402edeb3c7f32af70c780ff46a986b22e9a318d6
davidtaing <adavidtaing@gmail.com>
2023-04-02 21:48:09 +1000

    build: add dotenv to apps/cms

--- apps/cms/keystone.ts
+++ apps/cms/keystone.ts
@@ -1,7 +1,10 @@
 // Welcome to Keystone!
 //
 // This file is what Keystone uses as the entry-point to your headless backend
-//
+
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+
 // Keystone imports the default export of this file, expecting a Keystone configuration object
 //   you can find out more at https://keystonejs.com/docs/apis/config
 import { config } from "@keystone-6/core";
