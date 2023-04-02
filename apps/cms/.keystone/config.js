84c8dcf01caf45f77f8f4a4c94cd151e1765001b
davidtaing <adavidtaing@gmail.com>
2023-04-02 22:36:54 +1000

    remove: initFirstItem from auth.ts config

--- apps/cms/.keystone/config.js
+++ apps/cms/.keystone/config.js
@@ -161,17 +161,7 @@ var { withAuth } = (0, import_auth.createAuth)({
   //   this can be helpful for when you are writing your access control functions
   //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
   sessionData: "name createdAt",
-  secretField: "password",
-  // WARNING: remove initFirstItem functionality in production
-  //   see https://keystonejs.com/docs/config/auth#init-first-item for more
-  initFirstItem: {
-    // if there are no items in the database, by configuring this field
-    //   you are asking the Keystone AdminUI to create a new user
-    //   providing inputs for these fields
-    fields: ["name", "email", "password"]
-    // it uses context.sudo() to do this, which bypasses any access control you might have
-    //   you shouldn't use this in production
-  }
+  secretField: "password"
 });
 var sessionMaxAge = 60 * 60 * 24 * 30;
 var session = (0, import_session.statelessSessions)({
